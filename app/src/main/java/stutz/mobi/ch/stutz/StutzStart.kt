a63591dd38efdac9b8a3a7e693f1c90610c4c186
wemu <werner.mueller@monis.ch>
2018-06-06 11:52:42 +0200

    added search text and result list

--- app/src/main/java/stutz/mobi/ch/stutz/StutzStart.kt
+++ app/src/main/java/stutz/mobi/ch/stutz/StutzStart.kt
@@ -3,6 +3,7 @@ package stutz.mobi.ch.stutz
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 
 import kotlinx.android.synthetic.main.activity_stutz_start.*
 
@@ -11,6 +12,8 @@ class StutzStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stutz_start)
+
+        person_suche_resultat.adapter = ArrayAdapter<String>(this, R.layout.person_suche_resultat_layout, R.id.gefundenePerson, mutableListOf("Pauli", "Petra"))
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
