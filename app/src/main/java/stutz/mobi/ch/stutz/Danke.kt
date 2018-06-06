fcfd9bf793f5a63b11b3722ba89f52394f5fdb1e
Wampfler Rafael ( U114379 ) <rafael.wampfler@mobi.ch>
2018-06-06 16:30:39 +0200

    danke icon

--- app/src/main/java/stutz/mobi/ch/stutz/Danke.kt
+++ app/src/main/java/stutz/mobi/ch/stutz/Danke.kt
@@ -2,7 +2,6 @@ package stutz.mobi.ch.stutz
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_danke.*
@@ -14,11 +13,6 @@ class Danke : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_danke)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         neue_spende.setOnClickListener( {
             startActivity(Intent("stutz.mobi.ch.stutz.MAIN"))
