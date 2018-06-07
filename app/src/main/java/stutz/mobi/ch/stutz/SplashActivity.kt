7bfc068d9c9131a8ef6e4b958afe303bbd6a82e5
Wampfler Rafael ( U114379 ) <rafael.wampfler@mobi.ch>
2018-06-07 12:01:08 +0200

    mittagessen

---
+++ app/src/main/java/stutz/mobi/ch/stutz/SplashActivity.kt
@@ -0,0 +1,26 @@
+package stutz.mobi.ch.stutz
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        try {
+            // Simulate network access.
+            Thread.sleep(5000)
+        } catch (e: InterruptedException) {
+        }
+        startActivity(Intent(this, StutzStart::class.java))
+        finish()
+    }
+
+
+
+}
