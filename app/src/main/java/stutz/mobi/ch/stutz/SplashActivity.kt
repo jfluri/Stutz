26d73edf5b5928ae0ec8c409efccf277a1e7bd3f
wemu <werner.mueller@monis.ch>
2018-06-07 13:42:11 +0200

    minimal splash screen with logo

--- app/src/main/java/stutz/mobi/ch/stutz/SplashActivity.kt
+++ app/src/main/java/stutz/mobi/ch/stutz/SplashActivity.kt
@@ -2,6 +2,7 @@ package stutz.mobi.ch.stutz
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
@@ -9,16 +10,34 @@ import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3141
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, StutzStart::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        try {
-            // Simulate network access.
-            Thread.sleep(500)
-        } catch (e: InterruptedException) {
-        }
-        startActivity(Intent(this, StutzStart::class.java))
-        finish()
+
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
     }
 
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
 }
