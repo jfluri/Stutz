0f1d74b499addf499eac3ec1ae0bc811e2db5306
Wampfler Rafael ( U114379 ) <rafael.wampfler@mobi.ch>
2018-06-07 10:27:20 +0200

    schönere Spendenliste

--- app/src/main/java/stutz/mobi/ch/stutz/SpendenAdapter.kt
+++ app/src/main/java/stutz/mobi/ch/stutz/SpendenAdapter.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.person_suche_resultat_layout.view.*
 import kotlinx.android.synthetic.main.spende_suche_resultat_layout.view.*
-import java.security.AccessControlContext
 
 class SpendenAdapter: ArrayAdapter<Spende>  {
 
@@ -15,9 +13,11 @@ class SpendenAdapter: ArrayAdapter<Spende>  {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view = LayoutInflater.from(this.context).inflate(R.layout.spende_suche_resultat_layout, parent,false)
-        val dienstleisterId = this.getItem(position).dienstleisterId
+        val spende = this.getItem(position)
+        val dienstleisterId = spende.dienstleisterId
         val dienstleister = DienstleisterRepo().get(dienstleisterId)
-        view.gefundeneSpende.text = dienstleister.dienstleistung
+        view.dienstleistung.text = dienstleister.dienstleistung
+        view.person.text = PersonRepo().get(spende.personId).name
         return view
 
     }
